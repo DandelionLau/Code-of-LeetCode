d254100cb23a5769e91b43dcd206db843356d942
Danc1elion <liujieit@foxmail.com>
2019-08-06 20:42:35 +0800

    Update 94. Binary Tree Inorder Traversal.py

--- 94. Binary Tree Inorder Traversal.py
+++ 94. Binary Tree Inorder Traversal.py
@@ -10,3 +10,21 @@ class Solution:
             res.append(root.val)
             self.scan(root.right, res)
             
+            
+'''
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        stack = []
+        cur = root
+
+        while cur is not None or len(stack) > 0:
+            if cur is not None:
+                stack.append(cur)
+                cur = cur.left
+            else:
+                cur = stack.pop()
+                res.append(cur.val)
+                cur = cur.right
+        return res
+'''
